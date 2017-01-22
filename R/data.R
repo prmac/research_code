b316cffbf5c500ddf016d2b7ff75f29898169ee0
Paul McAdam <paulmcadam@Pauls-MacBook-Air.local>
2017-01-22 20:52:45 +0000

    fix example data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' 
+#' An example tree
+#'
+#' @format Phylogenetic tree with 379 tips and 378 internal nodes.
+#' @source NM398 paper
+#'
+"example_tree"
+
+
+#' 
+#' Example trait data
+#'
+#' @format Data frame with 3 columns and 393 rows
+#' @source NM398 paper
+#'
+"example_trait_data"
