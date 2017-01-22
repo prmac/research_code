9ba4db18446a7b27bfa4bbccae56fa0dfb128e7a
Paul McAdam <paulmcadam@Pauls-MacBook-Air.local>
2017-01-22 19:38:56 +0000

    first commit, add sm test code

---
+++ slatkin.maddison/R/run_sm.R
@@ -0,0 +1,20 @@
+#' Run SM test
+#'
+#' @param tree phylogeny to test
+#' @param data dataframe of trait data, first column must contain tip labels of phylogeny
+#' @param trait column name of trait of interest
+#' @param rep number of randomisations to perform
+#' run_sm()
+
+run_sm <- function(tree, data, trait, rep=999) {
+  tree <- drop.tip(tree, tree[["tip.label"]][! tree[["tip.label"]] %in% data[, 1]])
+  data <- data[data[,1] %in% tree[["tip.label"]], ]
+  tr4 <- phylo4d(tree, data, label.type="column")
+  pruned <- as(extractTree(tr4), "phylo")
+  ldata <- tdata(tr4, "tip")
+  ldata <- ldata[pruned[["tip.label"]], trait]
+  result <- sm_test(ldata, pruned, rep=rep)
+
+  result
+}
+
